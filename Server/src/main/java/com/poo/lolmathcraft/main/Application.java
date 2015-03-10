916b63d8a1ed8851d5d470e86283aef0f2541eb8
Dolan Miu <dolan_miu@hotmail.com>
2015-03-10 16:19:22 +0000

    fixed problems perhaps

--- Server/src/main/java/com/poo/lolmathcraft/main/Application.java
+++ Server/src/main/java/com/poo/lolmathcraft/main/Application.java
@@ -1,14 +1,15 @@
 package com.poo.lolmathcraft.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-//public class Application {
-//	public static void main(String[] args) {
-//		SpringApplication.run(Application.class, args);
-//	}
-//}
-//
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+
 //@ComponentScan
 //@EnableAutoConfiguration
 //public class Application {
