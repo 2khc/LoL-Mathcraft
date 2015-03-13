dff7713a23496e21d8b5e0b1da63634a9ffd68d3
Dolan Miu <dolan_miu@hotmail.com>
2015-03-13 16:55:58 +0000

    restructured with new yeoman

--- Client/app/scripts/main.js
+++
@@ -1,4 +0,0 @@
-/* jshint devel:true */
-/*global angular */
-
-angular.module('app', ['ng-annotate', 'ui.router']);
---
+++ Client/src/app/builder/routes.js
@@ -0,0 +1,20 @@
+angular.module('app').config(function ($stateProvider, $urlRouterProvider) {
+    'use strict';
+    $stateProvider.state('root', {
+        abstract: true,
+        name: '',
+        url: '^',
+        views: {
+            'root': {
+                templateUrl: 'client/components/root/partial.root.html'
+            },
+            'header@root': {
+                templateUrl: 'client/components/root/partial.header.html'
+            },
+            'footer@root': {
+                templateUrl: 'client/components/root/partial.footer.html'
+            }
+        }
+    });
+    //$urlRouterProvider.otherwise('home');
+});
