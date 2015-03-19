7e1476a6073681bd3d524b20aa9882f6931ca28a
unknown <dolan@Dolan-Zoo.adsm.local>
2015-03-19 13:52:14 +0000

    added ng dialog dep

--- Client/src/app/index.js
+++ Client/src/app/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mathCraft', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap', 'ui.select'])
+angular.module('mathCraft', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap', 'ui.select', 'ngDialog'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
