438be65c3817cca425b58cf7624c576b9d36b33b
Dolan Miu <dolan_miu@hotmail.com>
2015-03-22 22:00:06 +0000

    wrote an case insensitive inner join method

--- Client/src/components/utilities/services/arrayUtilService.js
+++ Client/src/components/utilities/services/arrayUtilService.js
@@ -2,16 +2,30 @@
 angular.module('mathCraft').service('arrayUtilService', function () {
     'use strict';
 
-    this.removeA = function (arr) {
-        var what, a = arguments,
-            L = a.length,
-            ax;
-        while (L > 1 && arr.length) {
-            what = a[--L];
-            while ((ax = arr.indexOf(what)) !== -1) {
-                arr.splice(ax, 1);
+    function getLowestArray(array1, array2) {
+        if (array1.length > array2.length) {
+            return array2;
+        } else {
+            return array1;
+        }
+    }
+
+    this.caseInsensitiveStringInnerJoin = function (array1, array2) {
+        var arrayIndex1, arrayIndex2, joinAmount, currentString, innerJoin = [];
+        if (!angular.isDefined(array1) || !angular.isDefined(array2)) {
+            return innerJoin;
+        }
+
+        for (arrayIndex1 = 0; arrayIndex1 < array1.length; arrayIndex1 += 1) {
+            joinAmount = 0;
+            currentString = array1[arrayIndex1].toLowerCase();
+
+            for (arrayIndex2 = 0; arrayIndex2 < array2.length; arrayIndex2 += 1) {
+                if (currentString === array2[arrayIndex2].toLowerCase()) {
+                    innerJoin.push(array2[arrayIndex2]);
+                }
             }
         }
-        return arr;
-    }
+        return innerJoin;
+    };
 });
