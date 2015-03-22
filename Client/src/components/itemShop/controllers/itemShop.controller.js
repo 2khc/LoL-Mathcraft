e58b5575c8794a50842cd2f721b1ea8882b9a278
Dolan Miu <dolan_miu@hotmail.com>
2015-03-22 22:00:22 +0000

    added filtering

--- Client/src/components/itemShop/controllers/itemShop.controller.js
+++ Client/src/components/itemShop/controllers/itemShop.controller.js
@@ -1,15 +1,51 @@
 /*global angular */
-angular.module('mathCraft').controller('itemShopController', function ($scope, itemService, jsonUtilService) {
+angular.module('mathCraft').controller('itemShopController', function ($scope, itemService, jsonUtilService, arrayUtilService, lodash) {
     'use strict';
 
-    console.log($scope.ngDialogData);
+    $scope.searchTagFilter = [];
+    $scope.filterResults = [];
+    $scope.searchResults = [];
 
     itemService.getAllItemInfo().then(function (data) {
         $scope.allItems = jsonUtilService.convertToArray(data);
     });
 
+    itemService.getTree().then(function (data) {
+        $scope.itemTree = data;
+        console.log(data);
+    });
+
+    function filter(tags) {
+        var searchIndex, intersectedItems, filterResults = [];
+        for (searchIndex = 0; searchIndex < $scope.allItems.length; searchIndex += 1) {
+            //intersectedItems = lodash.intersection($scope.allItems[searchIndex].tags, tags);
+            intersectedItems = arrayUtilService.caseInsensitiveStringInnerJoin($scope.allItems[searchIndex].tags, tags);
+            console.log(intersectedItems);
+            if (intersectedItems.length === tags.length) {
+                console.log($scope.allItems[searchIndex]);
+                filterResults.push($scope.allItems[searchIndex]);
+            }
+        }
+        return filterResults;
+    }
+
     $scope.addItem = function (index) {
         $scope.ngDialogData.push($scope.allItems[index]);
+    };
+
+    $scope.changeFilter = function (tag, checked) {
+        if (checked) {
+            $scope.searchTagFilter.push(tag);
+        } else {
+            lodash.remove($scope.searchTagFilter, function (n) {
+                return n === tag;
+            });
+        }
+        if ($scope.searchTagFilter.length === 0) {
+            return;   
+        }
+        $scope.filterResults = filter($scope.searchTagFilter);
+        //console.log($scope.filterResults);
     };
 
     $scope.search = function () {
