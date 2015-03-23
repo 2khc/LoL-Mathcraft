bd2e62f9a81223fd9d98104d973af047f08936a5
Dolan Miu <dolan_miu@hotmail.com>
2015-03-23 02:47:03 +0000

    refactored to work with items too

--- LoL Icon Generator/src/com/poo/lolicon/Main.java
+++ LoL Icon Generator/src/com/poo/lolicon/Main.java
@@ -12,25 +12,40 @@ import org.json.simple.parser.ParseException;
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+		extractItems();
+		extractChampions();
+	}
+
+	private static void extractItems() throws FileNotFoundException, IOException, ParseException {
+		Path currentRelativePath = Paths.get("");
+		String s = currentRelativePath.toAbsolutePath().toString();
+
+		FileFinder fileFinder = new FileFinder(s + "/images");
+		FileRenamer fileRenamer = new FileRenamer(Paths.get("dist").toString());
+
+		extraction(s + "/json.txt", "data", newObjects -> {
+			System.out.println(newObjects[1] + ": " + newObjects[0]);
+			findAndRename(fileFinder, fileRenamer, newObjects, (name) -> {
+				String formattedString = ItemFormatter.replaceUnderscoreWithSpace(name);
+				return formattedString.split("\\s+")[0];
+			});
+		}, "name", "id");
+	}
+
+	private static void extractChampions() throws FileNotFoundException, IOException, ParseException {
 		Path currentRelativePath = Paths.get("");
 		String s = currentRelativePath.toAbsolutePath().toString();
 
 		FileFinder fileFinder = new FileFinder(s + "/champimages");
 		FileRenamer fileRenamer = new FileRenamer(Paths.get("dist").toString());
 
-		/*extraction(Paths.get("images").toString(), s + "/json.txt", Paths.get("dist").toString(), "data", newObjects -> {
-			System.out.println(newObjects[1] + ": " + newObjects[0]);
-			findAndRename(fileFinder, fileRenamer, newObjects);
-		}, "name", "id");*/
-		
 		extraction(s + "/champjson.txt", "data", newObjects -> {
-			//System.out.println(newObjects[1] + ": " + newObjects[0]);
-			findAndRename(fileFinder, fileRenamer, newObjects, (name) -> {
-				String splited = ItemFormatter.splitCamelCase(name);
-				return splited.split("\\s+")[0];
-				//return splited;
-			});
-		}, "name", "id");
+			// System.out.println(newObjects[1] + ": " + newObjects[0]);
+				findAndRename(fileFinder, fileRenamer, newObjects, (name) -> {
+					String splited = ItemFormatter.splitCamelCase(name);
+					return splited.split("\\s+")[0];
+				});
+			}, "name", "id");
 	}
 
 	private static void extraction(String pathToJson, String jsonRoot, ObjectExtractCallback callback, String... fields) throws FileNotFoundException, IOException, ParseException {
@@ -44,7 +59,7 @@ public class Main {
 
 	private static void findAndRename(FileFinder fileFinder, FileRenamer fileRenamer, Object[] objects, FormatterFunction function) {
 		String formattedStr = function.format((String) objects[0]);
-		//System.out.println(formattedStr);
+		// System.out.println(formattedStr);
 		File[] files = fileFinder.search(formattedStr);
 		if (files.length == 0) {
 			System.out.println(formattedStr + " could not be found in JSON.");
