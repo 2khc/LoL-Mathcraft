4be29c2a7cf2faa0723be55750ee0a20cec8902d
unknown <dolan@Dolan-Zoo.adsm.local>
2015-03-23 09:14:37 +0000

    fixed duplicate item bug

--- Client/src/components/itemShop/controllers/itemShop.controller.js
+++ Client/src/components/itemShop/controllers/itemShop.controller.js
@@ -2,13 +2,22 @@
 angular.module('mathCraft').controller('itemShopController', function ($scope, itemService, calculateService, jsonUtilService) {
     'use strict';
 
-    console.log($scope.ngDialogData);
-
     itemService.getAllItemInfo().then(function (data) {
         $scope.allItems = jsonUtilService.convertToArray(data);
     });
 
     $scope.addItem = function (index) {
+        if ($scope.ngDialogData.length >= 6) {
+            return false;
+        }
+
+        console.log($scope.allItems[index]);
         $scope.ngDialogData.push($scope.allItems[index]);
-    }
+        console.log($scope.ngDialogData);
+        return true;
+    };
+
+    $scope.removeItem = function (index) {
+        $scope.ngDialogData.splice(index, 1);
+    };
 });
